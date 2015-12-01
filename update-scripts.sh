d87d558fcea52c2dfe88f2a6d4feecbb4eb4a7d3
Geert Bongers <email@geertbongers>
2015-12-01 14:20:34 +0100

    Updated main update script

--- update-scripts.sh
+++ update-scripts.sh
@@ -17,7 +17,7 @@ if [ -f "./master.zip" ]; then
         logger "Synology Scripts - Downloading master zip"
         rm -Rf ./synology-scripts
         mv ./synology-scripts-master ./synology-scripts
-        mkdir ./update-synology-scripts
+        mkdir -p ./update-synology-scripts
     else
         logger -p local0.err "Synology Scripts - Unzipping failed download master.zip"
     fi
