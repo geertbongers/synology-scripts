b14868448c98d1fd2945230d97b93ca6f8d36aa1
Geert Bongers <email@geertbongers>
2015-12-01 13:53:18 +0100

    Updated main update script

--- update-scripts.sh
+++ update-scripts.sh
@@ -5,15 +5,17 @@
 logger "Synology Scripts - Updating synology scripts from github"
 logger "Synology Scripts - Go to upper directory and download latest master"
 cd .. && wget https://github.com/geertbongers/synology-scripts/archive/master.zip
-if ls ./master.zip > /dev/null; then
+if [ ls ./master.zip > /dev/null ]; then
     logger "Synology Scripts - Unzipping master zip"
+    rm -Rf ./synology-scripts-master
     unzip master.zip
-    if ls ./synology-scripts-master > /dev/null; then
+    if [ ls ./synology-scripts-master > /dev/null ]; then
         logger "Synology Scripts - Downloading master zip"
+        mv ./synology-scripts-master ./synology-scripts
     else
         logger -p local0.err "Synology Scripts - Unzipping failed download master.zip"
-        rm master.zip
     fi
+    rm master.zip
 else
     logger -p local0.err "Synology Scripts - Failed download master.zip"
 fi
