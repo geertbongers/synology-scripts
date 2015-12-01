3406a3a6f08dae66e906dd9bbeeb4ebfc2f7f2cd
Geert Bongers <email@geertbongers>
2015-12-01 13:45:57 +0100

    Modified file/directory exists

--- update-scripts.sh
+++ update-scripts.sh
@@ -5,10 +5,10 @@
 logger "Synology Scripts - Updating synology scripts from github"
 logger "Synology Scripts - Go to upper directory and download latest master"
 cd .. && wget https://github.com/geertbongers/synology-scripts/archive/master.zip
-if [ -f ./master.zip ]; then
+if [ -f "./master.zip" ]; then
     logger "Synology Scripts - Unzipping master zip"
     unzip master.zip
-    if [ -d ./synology-scripts-master ]; then
+    if [ -d "./synology-scripts-master" ]; then
         logger "Synology Scripts - Downloading master zip"
     else
         logger -p local0.err "Synology Scripts - Unzipping failed download master.zip"
