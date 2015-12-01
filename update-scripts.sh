9e4a6f494d352f2a01c2b82bf092c160127151a8
Geert Bongers <email@geertbongers>
2015-12-01 13:49:51 +0100

    Modified file/directory exists for sh instead of bash

--- update-scripts.sh
+++ update-scripts.sh
@@ -5,10 +5,10 @@
 logger "Synology Scripts - Updating synology scripts from github"
 logger "Synology Scripts - Go to upper directory and download latest master"
 cd .. && wget https://github.com/geertbongers/synology-scripts/archive/master.zip
-if [ -f "./master.zip" ]; then
+if ls ./master.zip > /dev/null; then
     logger "Synology Scripts - Unzipping master zip"
     unzip master.zip
-    if [ -d "./synology-scripts-master" ]; then
+    if ls ./synology-scripts-master > /dev/null; then
         logger "Synology Scripts - Downloading master zip"
     else
         logger -p local0.err "Synology Scripts - Unzipping failed download master.zip"
