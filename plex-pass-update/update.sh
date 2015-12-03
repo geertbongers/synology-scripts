64b3d1ea797591d6a316d0ff5b06b3a7f5a3a8a2
Geert Bongers <email@geertbongers>
2015-12-03 14:48:24 +0100

    Renamed package to original name, added auto-upgrade stuff

--- plex-pass-update/update.sh
+++ plex-pass-update/update.sh
@@ -91,6 +91,7 @@ if [ -f "${DOWNLOADDIR}/${FILENAME}" -a "${FORCE}" != "yes" ]; then
 	tar -xvf "${DOWNLOADDIR}/${FILENAME}" -C ./plex-temp
 	echo "silent_upgrade=true" >> ./plex-temp/INFO
 	tar -cvf "${FILENAME}" -C ./plex-temp .
+	rm -Rf ./plex-temp
 	mv "${FILENAME}" "/volume1/web/sspks/packages/plex_bromolow_${VERSION}.spk"
 	cp /volume1/applications/synology-scripts/plex-pass-update/plex.nfo /volume1/applications/synology-scripts/plex-pass-update/plex-new.nfo
 	echo "version=\"${VERSION}\"" >> /volume1/applications/synology-scripts/plex-pass-update/plex-new.nfo
