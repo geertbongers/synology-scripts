b3d5d33b4318205e2712dd1c81576929062bbe58
Geert Bongers <email@geertbongers>
2015-12-03 15:49:29 +0100

    Renamed package to original name, added auto-upgrade stuff

--- sspks/update.sh
+++ sspks/update.sh
@@ -1,8 +1,8 @@
 cd /volume1/web
 wget https://github.com/geertbongers/sspks/archive/plexconnect.zip
 unzip plexconnect.zip
-rm -Rf sspks-plexconnect/packages/*
-cp sspks/packages/* sspks-plexconnect/packages
-rm -Rf sspks
-mv sspks-plexconnect/ sspks
+rm -Rf ./sspks-plexconnect/packages/*
+cp ./sspks/packages/* ./sspks-plexconnect/packages
+rm -Rf ./sspks
+mv ./sspks-plexconnect ./sspks
 rm plexconnect.zip
